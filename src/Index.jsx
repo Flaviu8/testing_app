af56d20c344fc9ad8c04774f4727b3ac82b054d6
Flaviu10 <flavius_flaviu93@yahoo.com>
2025-03-31 16:20:55 +0100

    align items in center

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Index() {
   return (
     <div>
-        <Link to="/dashboard"/>
+        <NavLink to="/dashboard" >Dashboard</NavLink>
+        Hello
     </div>
   )
 }
